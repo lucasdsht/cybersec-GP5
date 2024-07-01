01ecd87fd19a9638dde0e0f41b6b9ccef4494071
distrib-ps <theodelhay@orange.fr>
2024-07-01 10:17:00 +0200

    feat: login + register + route

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,29 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import Register from './Register';
+import Login from './Login';
 import './App.css';
+
+function Home() {
+  return (
+    <div>
+      <h1>Accueil</h1>
+      <nav>
+        <Link to="/login">Connexion</Link>
+        <br />
+        <Link to="/register">Inscription</Link>
+      </nav>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <h1>Register</h1>
-      <Register />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
